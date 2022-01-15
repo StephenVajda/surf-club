7aa601868c26e3b26395390d8eb7e2920b539a3d
Stephen Vajda <sgv@rockwoodpl.com>
2022-01-15 17:30:18 -0500

    added and changed posts routes

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const createError = require('http-errors');
 const express = require('express');
 const app=express();
 const path = require('path');
+//const bodyParser=require('body-parser');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const index = require('./routes/index');
+const posts = require('./routes/posts');
+const users = require('./routes/users');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -16,10 +18,12 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+//app.use(bodyParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', index);
+app.use('/posts', posts);
+app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
